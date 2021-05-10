d00c6428358a8e123c6a122112c338ef5275f452
Jamros <jamros-kamil@wp.pl>
2021-05-10 21:28:10 +0200

    Create ISE project

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "F:/FPGA/UC_LED_WYSWEITLACZ_V2/uC_led_wyswietlacz/test_isim_beh.exe" -prj "F:/FPGA/UC_LED_WYSWEITLACZ_V2/uC_led_wyswietlacz/test_beh.prj" "work.test" 
